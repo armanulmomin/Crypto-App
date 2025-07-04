7db3ebb41013fe2eef98ec143adbf4e293e40f1f
Armanul Momin <armanul.momin@gmail.com>
2025-07-04 20:09:42 +1000

    working on coin list view

---
+++ Crypto App/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  Crypto App
+//
+//  Created by Arman on 4/7/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    
+    CoinRowView(coin: DeveloperPreview.instance.coin)
+    
+    
+}
