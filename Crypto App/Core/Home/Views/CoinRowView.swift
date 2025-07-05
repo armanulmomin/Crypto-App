01760ed8d566e8a499a7c151b1ff1b1432355bd1
Armanul Momin <armanul.momin@gmail.com>
2025-07-05 19:55:12 +1000

    Coin List view updated

--- Crypto App/Core/Home/Views/CoinRowView.swift
+++ Crypto App/Core/Home/Views/CoinRowView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 struct CoinRowView: View {
     let coin: CoinModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0){
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30,height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading,6)
+                .foregroundStyle(Color.theme.accent)
+            Spacer()
+            VStack{
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundStyle(Color.theme.accent)
+                
+                
+            }
+        }
     }
 }
 
